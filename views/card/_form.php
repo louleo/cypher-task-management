a2143436604bee1600dc430e5442569224e562a3
louleo <louleolin@gmail.com>
2019-10-10 21:25:55 +1100

    CP-7 Remove start and end date in card creation.

--- views/card/_form.php
+++ views/card/_form.php
@@ -34,10 +34,6 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'due_date')->textInput() ?>
 
-    <?= $form->field($model, 'start_date')->textInput() ?>
-
-    <?= $form->field($model, 'end_date')->textInput() ?>
-
     <?= $form->field($model, 'github_pr_link')->textInput() ?>
 
     <?= $form->field($model, 'total_used_time')->hiddenInput(['maxlength' => true])->label(false) ?>
